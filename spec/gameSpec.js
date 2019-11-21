a805613b47d2fd572a2bc841a36223a7011c95de
robertamangiapane <roberta.mangiapane@gmail.com>
2019-11-21 17:35:46 +0000

    implement result calculation

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,92 +7,95 @@ describe("Game", function(){
 
   describe("claim_field", function(){
     it("let player X claim a field if is available", function(){
-      game.turn = "X"
-
-      expect(game.claim_field(1)).toEqual("Turn ended.")
-    })
-
-    it("field is claimed by the player X", function(){
+      game.fields = ["O", "", "", "", "", "", "", "", ""]
       game.turn = "X"
       game.claim_field(1)
 
       expect(game.fields[1]).toEqual("X")
     })
 
-    it("throw an error if the field is already taken bu PlayerX", function(){
-      game.fields[1] = "X"
+    it("throw an error if the field is already taken by PlayerX", function(){
+      game.fields = ["X", "", "", "", "", "", "", "", ""]
+      game.turn = "O"
 
-      expect(function(){game.claim_field(1)}).toThrow('Field already taken by PlayerX')
+      expect(function(){game.claim_field(0)}).toThrow('Field already taken by PlayerX')
     })
 
     it("throw an error if the field is already taken bu PlayerO", function(){
-      game.fields[1] = "O"
+      game.fields = ["O", "", "", "", "", "", "", "", ""]
+      game.turn = "X"
 
-      expect(function(){game.claim_field(1)}).toThrow('Field already taken by PlayerO')
+      expect(function(){game.claim_field(0)}).toThrow('Field already taken by PlayerO')
     })
   })
-
+  
   describe("result", function(){
     it("return playerX win", function(){
-      game.playerX.result = "win!"
-      game.playerO.result = "lose!"
-      game.gameover = true
+      game.fields = ["X", "X", "",
+                     "", "", "",
+                     "", "", ""]
+      game.turn = "X"
+      game.claim_field(2)
+
 
       expect(game.result()).toEqual("PlayerX win! PlayerO lose! Game Over!")
     })
 
     it("return playerO win", function(){
-      game.playerO.result = "win!"
-      game.playerX.result = "lose!"
-      game.gameover = true
+      game.fields = ["O", "", "",
+                     "", "O", "",
+                     "", "", ""]
+      game.turn = "O"
+      game.claim_field(8)
+
 
       expect(game.result()).toEqual("PlayerX lose! PlayerO win! Game Over!")
     })
 
     it("return a tie", function(){
-      game.playerO.result = ""
-      game.playerX.result = ""
-      game.gameover = true
+      game.fields = ["O", "X", "O",
+                     "O", "X", "X",
+                     "X", "O", ""]
+      game.turn = "O"
+      game.claim_field(8)
+
 
       expect(game.result()).toEqual("It's a tie. Game Over!")
     })
+  })
 
-    it("return the game is not over yet", function(){
-      game.playerO.result = ""
-      game.playerX.result = ""
-      game.gameover = false
+  describe("_set_first_turn", function(){
+    it("start the game with random turn for PlayerX", function(){
+      spyOn(game, '_random_turn').and.returnValue("X")
+      game._set_first_turn()
 
-      expect(game.result()).toEqual("The game is not over yet!")
+      expect(game.turn).toEqual("X")
+    })
+
+    it("start the game with random turn for PlayerO", function(){
+      spyOn(game, '_random_turn').and.returnValue("O")
+      game._set_first_turn()
+
+      expect(game.turn).toEqual("O")
     })
   })
 
   describe("_switch_player_turn", function(){
     it("switch the turn of the PlayerX", function(){
       game.turn = "X"
+      game._switch_player_turn()
 
-      expect(game._switch_player_turn()).toEqual("O")
+      expect(game.turn).toEqual("O")
 
     })
 
     it("switch the turn of the PlayerO", function(){
       game.turn = "O"
+      game._switch_player_turn()
 
-      expect(game._switch_player_turn()).toEqual("X")
+      expect(game.turn).toEqual("X")
     })
 
   })
 
-  describe("_set_first_turn", function(){
-    it("start the game with random turn for PlayerX", function(){
-      spyOn(game, '_random_turn').and.returnValue("X")
-
-      expect(game._set_first_turn()).toEqual("X")
-    })
-
-    it("start the game with random turn for PlayerO", function(){
-      spyOn(game, '_random_turn').and.returnValue("O")
-
-      expect(game._set_first_turn()).toEqual("O")
-    })
-  })
 })
--- src/game.js
+++ src/game.js
@@ -5,14 +5,32 @@ function Game(){
   this.fields = ["", "", "", "", "", "", "", "", ""]
   this.turn = ""
   this.players = ["X", "O"]
+  this.rows_columns = []
+
 }
 
-Game.prototype._set_first_turn = function(){
-  this.turn = this._random_turn()
-  return this.turn
+Game.prototype.claim_field = function(index){
+  console.log(this.fields[1])
+  console.log("this field index inside claim field", this.fields[index])
+  if(this.fields[index] === "X" || this.fields[index] === "O") {
+    throw 'Field already taken by Player' + this.fields[index]
+  } else {
+    this._set_taken_field(index)
+    this._check_match()
+    this._check_gameover()
+  }
+}
+
+Game.prototype._check_gameover = function(){
+  if(this.gameover === true){
+    this.result()
+  } else {
+    this._switch_player_turn()
+  }
 }
 
 Game.prototype.result = function(){
+
   if (this.gameover === false){
     return "The game is not over yet!"
   } else if (this.gameover === true && this.playerX.result === "" && this.playerO.result === ""){
@@ -22,32 +40,115 @@ Game.prototype.result = function(){
   }
 }
 
+Game.prototype._check_match = function(){
+  result = []
+  this._rows_columns_matrix()
 
-Game.prototype.claim_field = function(index){
+  this.rows_columns.forEach(function(row_column){
+    result.push(this._countInArray(row_column, this.turn))
+  }, this)
 
-  if(this.fields[index] === "X" || this.fields[index] === "O") {
-    throw 'Field already taken by Player' + this.fields[index]
-  } else {
-    this._set_taken_field(index)
-    this._switch_player_turn()
-    return "Turn ended."
+  if(result.includes(3)){
+    this.gameover = true
+    this._check_player_result()
+  }else if(!result.includes(3)){
+    if(!this.fields.includes("")){
+      this.gameover = true
+      this.playerX.result = ""
+      this.playerO.result = ""
+    }
   }
 }
 
-Game.prototype._switch_player_turn = function(){
-  if(this.turn === "X"){
-    return this.turn = "O"
-  } else {
-    return this.turn = "X"
-  }
-}
-
-Game.prototype._set_taken_field = function(index){
-  this.fields[index] = this.turn
-
+Game.prototype._set_first_turn = function(){
+  this.turn = this._random_turn()
+  this.turn
 }
 
 Game.prototype._random_turn = function(){
   player = ~~(Math.random() * this.players.length());
   return this.players[player];
+}
+
+Game.prototype._set_taken_field = function(index){
+  console.log("this fields", this.fields)
+  console.log("this field index", this.fields[index])
+  this.fields[index] = this.turn
+}
+
+Game.prototype._switch_player_turn = function(){
+  if(this.turn === "X"){
+    this.turn = "O"
+  } else {
+    this.turn = "X"
+  }
+}
+
+Game.prototype._check_player_result = function(){
+  if(this.turn === "X"){
+    this.playerX.result = "win!"
+    this.playerO.result = "lose!"
+  } else if(this.turn === "O"){
+    this.playerO.result = "win!"
+    this.playerX.result = "lose!"
+  }
+}
+
+Game.prototype._rows_columns_matrix = function(){
+  fields = this.fields.slice(0)
+  fields_matrix = []
+  while(fields.length){
+    fields_matrix.push(fields.splice(0,3))
+  }
+
+  c = 0
+  max = fields_matrix.length - 1
+
+  row1 = []
+  row2 = []
+  row3 = []
+
+  column1 = []
+  column2 = []
+  column3 = []
+
+  diagonal1 = []
+  diagonal2 = []
+
+  i = 0
+  for(c = 0; c <= max; c++){
+    row1.push(fields_matrix[i][c])
+    column1.push(fields_matrix[c][i])
+  }
+
+  i = 1
+  for(c = 0; c <= max; c++){
+    row2.push(fields_matrix[i][c])
+    column2.push(fields_matrix[c][i])
+  }
+
+  i = 2
+  for(c = 0; c <= max; c++){
+    row3.push(fields_matrix[i][c])
+    column3.push(fields_matrix[c][i])
+  }
+
+  for(c = 0; c <= max; c++){
+    diagonal1.push(fields_matrix[c][c])
+    diagonal2.push(fields_matrix[c][max-c])
+  }
+
+  this.rows_columns.push(row1, row2, row3, column1, column2, column3, diagonal1, diagonal2)
+
+}
+
+Game.prototype._countInArray = function(array, value){
+
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {
+      if (array[i] === value) {
+          count++;
+      }
+  }
+  return count;
 }
