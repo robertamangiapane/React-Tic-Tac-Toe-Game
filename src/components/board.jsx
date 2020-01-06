900360cdb8659ec7eecbd9818cb815af387f7193
robertamangiapane <roberta.mangiapane@gmail.com>
2020-01-06 15:55:48 +0000

    add iterating grid creation

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -36,6 +36,17 @@ class Board extends Component {
     }
   }
 
+  createGrid(){
+    var grid = []
+
+    for (let i = 0; i < 9; i++) {
+      grid.push(<button onClick={() => this.claimField(i)}>
+      {this.state.fields[i]}
+      </button>)
+    }
+    return grid
+  }
+
   render(){
     return(
       <div>
@@ -45,36 +56,8 @@ class Board extends Component {
         <div className="game-result">
         <Result result={this.state.result}/>
         </div>
-        <div className="grid">
         <div className="board-grid">
-          <button onClick={() => this.claimField(0)}>
-          {this.state.fields[0]}
-          </button>
-          <button onClick={() => this.claimField(1)}>
-          {this.state.fields[1]}
-          </button>
-          <button onClick={() => this.claimField(2)}>
-          {this.state.fields[2]}
-          </button>
-          <button onClick={() => this.claimField(3)}>
-          {this.state.fields[3]}
-          </button>
-          <button onClick={() => this.claimField(4)}>
-          {this.state.fields[4]}
-          </button>
-          <button onClick={() => this.claimField(5)}>
-          {this.state.fields[5]}
-          </button>
-          <button onClick={() => this.claimField(6)}>
-          {this.state.fields[6]}
-          </button>
-          <button onClick={() => this.claimField(7)}>
-          {this.state.fields[7]}
-          </button>
-          <button onClick={() => this.claimField(8)}>
-          {this.state.fields[8]}
-          </button>
-        </div>
+          {this.createGrid()}
         </div>
       </div>
 
